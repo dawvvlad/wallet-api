70085849c6c88424049d23bcd1c0821eaeff913f
vgolikov <vgolikov@btlab.ru>
2025-12-08 22:08:55 +0300

    first

---
+++ src/main/java/org/dawvvlad/walletapp/controller/GetWalletController.java
@@ -0,0 +1,9 @@
+package org.dawvvlad.walletapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wallets")
+public class GetWalletController {
+}
